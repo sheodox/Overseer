f39223390354eb8514e4c6401e9ad952ab877c2d
sheodox <imagamer27@msn.com>
2016-10-15 17:54:48 -0500

    use object spread to pass light group state as props, only export List, move the slider to a BrightnessSlider class to encapsulate the added complexity of properly updating when someone else changes brightness (defaultValue only set the initial value when the page loads, needed a way to update the actual value when the update came from someone else)

--- jsx/List.jsx
+++ jsx/List.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var List = React.createClass({
     render: function() {
         var groups = this.props.states.map((s, i) => {
-            return <LightGroup id={s.id} name={s.name} on={s.on} socket={this.props.socket} brightness={s.brightness} key={i}/>
+            return <LightGroup {...s} socket={this.props.socket} key={i}/>
         });
         return (
             <ul>
@@ -27,43 +27,95 @@ var LightGroup = React.createClass({
             detailsExpanded: !this.state.detailsExpanded
         });
     },
-    changeBrightness: function(e) {
-        //don't want to try to adjust the brightness on every increment
-        if (!this.state.updateTimeout) {
-            e.persist();
-            this.setState({
-                updateTimeout: setTimeout(() => {
-                    this.props.socket.emit('brightness', this.props.id, e.target.value);
-                    
-                    //allow later updates
-                    this.setState({updateTimeout: null});
-                }, 500)
-            });
-        }
-    },
     render: function() {
         var check = this.props.on ? '☑' : '☐',
-            slideId = 'lg-slide-' + this.props.id,
             toggleClass = 'lg-toggle ' + (this.props.on ? 'on' : 'off'),
             detailsClass = 'lg-details' + (this.state.detailsExpanded ? ' expanded' : '');
-        
+
         return (
             <li>
                 <div className="light-group">
                     <button className={toggleClass} onClick={this.toggle}>{check} {this.props.name}</button>
                     <button className="lg-details-toggle" onClick={this.toggleDetails}>⚙</button>
                     <div className={detailsClass}>
-                        <label htmlFor={slideId}>Brightness</label>
-                        <br/>
-                        <input className="lg-brightness-slide" id={slideId} type="range" min="0" max="255" onChange={this.changeBrightness} defaultValue={this.props.brightness}/>
+                        <BrightnessSlider {...this.props} />
                     </div>
-                    
                 </div>
             </li>
         )
     }
 });
 
-module.exports = {
-    List, LightGroup
-};
+var BrightnessSlider = React.createClass({
+    getInitialState: function() {
+        return {
+            adjusting: false,
+            updateTimeout: null,
+            //signifies we have changes in the dom we haven't received back from props (brightness has been changed but the server isn't broadcasting it yet)
+            hotChanges: false
+        };
+    },
+    changeBrightness: function(e) {
+        //don't want to try to adjust the brightness on every increment
+        if (!this.state.updateTimeout) {
+            e.persist();
+            this.setState({
+                hotChanges: true,
+                updateTimeout: setTimeout(() => {
+                    this.props.socket.emit('brightness', this.props.id, e.target.value);
+
+                    //allow later updates
+                    this.setState({updateTimeout: null});
+                }, 500)
+            });
+        }
+    },
+    componentWillReceiveProps: function() {
+        //we got a new state from the server, don't need to prevent setting the input value
+        this.setState({
+            hotChanges: false
+        });
+    },
+    startAdjust: function() {
+        this.setState({adjusting: true});
+    },
+    stopAdjust: function() {
+        this.setState({adjusting: false});
+    },
+    componentDidUpdate: function() {
+        //wait until we have a proper update from the server (not dragging the input and we have new props) so we don't get janky movement
+        if (!this.state.hotChanges && !this.state.adjusting) {
+            console.log(`wasn't adjusting`);
+            this.slider.value = this.props.brightness;
+        }
+        else {
+            console.log('was adjusting');
+        }
+    },
+    render: function() {
+        var inputAttrs = {
+                type: 'range',
+                min: '0',
+                max: '254',
+                id: 'lg-slide-' + this.props.id,
+                defaultValue: this.props.brightness
+            },
+            events = {
+                onChange: this.changeBrightness,
+                onMouseDown: this.startAdjust,
+                onMouseUp: this.stopAdjust,
+                onTouchStart: this.startAdjust,
+                onTouchEnd: this.stopAdjust
+            };
+
+        return (
+            <div>
+                <label htmlFor={inputAttrs.id}>Brightness</label>
+                <br/>
+                <input className="lg-brightness-slide" ref={c => this.slider = c} {...inputAttrs} {...events} />
+            </div>
+        )
+    }
+});
+
+module.exports = List;
