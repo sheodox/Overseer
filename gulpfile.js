b96081161331b1f7edf880875682052a115e4540
sheodox <imagamer27@msn.com>
2016-10-18 06:14:42 -0500

    restructured so everything can be compiled and use features that need babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,61 @@
-var gulp = require("gulp"),
-    babelify = require('babelify'),
-    browserify = require("browserify"),
-    connect = require("gulp-connect"),
+'use strict';
+var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    source = require("vinyl-source-stream");
+    babel = require('gulp-babel'),
+    webpack = require('gulp-webpack'),
+    sourcemaps = require('gulp-sourcemaps'),
+    jsGlob = './src/**/*.js',
+    webpackEntry = './dist/components/index.js',
+    sassGlob = './src/scss/**/*.scss',
+    uncompiledGlob = './src/**/*.!(js|scss)';
 
-
-gulp.task("build:js", function(){
-    return browserify({
-        entries: ["./jsx/index.js"],
-        extensions: [".js", ".jsx"]
-    })
-        .transform(babelify.configure({
-            presets : ["es2015", "react"]
+gulp.task('run:js', function() {
+    return gulp.src(jsGlob)
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['react', 'es2015']
         }))
-        .bundle()
-        .pipe(source("bundle.js"))
-        .pipe(gulp.dest("./public/javascripts"));
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build:scss', function() {
-    return gulp.src('./scss/**/*.scss')
+gulp.task('run:webpack', function() {
+    gulp.src(webpackEntry)
+        .pipe(webpack({
+            output: {filename: '[name].js'}
+        }))
+        .pipe(gulp.dest('./dist/public/js'));
+});
+
+gulp.task('run:scss', function() {
+    gulp.src(sassGlob)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/stylesheets'));
+        .pipe(gulp.dest('./dist/public/css'));
+});
+
+gulp.task('run:uncompiled', function() {
+    gulp.src(uncompiledGlob)
+        .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('watch:js', function() {
-    return gulp.watch('./jsx/**/*.*', ['build:js']);
+    gulp.watch(jsGlob, ['run:js']);
 });
 
 gulp.task('watch:scss', function() {
-    return gulp.watch('./scss/**/*.scss', ['build:scss']);
+    gulp.watch(sassGlob, ['run:scss']);
 });
+
+gulp.task('watch:uncompiled', function() {
+    gulp.watch(uncompiledGlob, ['run:uncompiled']);
+});
+
+gulp.task('watch:webpack', function() {
+    gulp.watch(webpackEntry, ['run:webpack']);
+});
+
+gulp.task('js', ['run:js', 'watch:js']);
+gulp.task('sass', ['run:scss', 'watch:scss']);
+gulp.task('uncompiled', ['run:uncompiled', 'watch:uncompiled']);
+gulp.task('webpack', ['run:webpack', 'watch:webpack']);
+gulp.task('run-all', ['js', 'webpack', 'sass', 'uncompiled']);
