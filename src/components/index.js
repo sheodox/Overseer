da7fdf61969f2a0b00f980dd136504410781a968
sheodox <imagamer27@msn.com>
2016-11-09 18:56:01 -0600

    added Settings and Voter

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+window.socket = io();
 import ReactDOM from 'react-dom';
 import React from 'react';
 import {Router, Route, browserHistory} from 'react-router';
@@ -5,10 +6,12 @@ import {Provider} from 'react-redux';
 import App from './App';
 import Lights from './Lights';
 import Games from './Games';
+import Voter from './Voter';
+import Settings from './Settings';
 import store from '../reducers/reducers';
 import appActions from '../actions/act-app-client';
-
-window.socket = io();
+import settingsActions from '../actions/act-settings-client';
+import voterActions from '../actions/act-voter-client';
 
 // socket.on('reconnect', () => {location.reload();});
 
@@ -19,12 +22,36 @@ socket.on('connect', () => {
     store.dispatch(appActions.socketConnected());
 });
 
+let availableSessionId = /sessionId=(\w*)/.exec(document.cookie)[1];
+store.dispatch(settingsActions.setSessionId(
+    localStorage.getItem('sessionId') || availableSessionId
+));
+
+
+let storedUsername = localStorage.getItem('username');
+if (storedUsername) {
+    let sessionId = store.getState().settings.sessionId;
+    settingsActions.propose(sessionId, storedUsername);
+}
+
+socket.on('settings/usernameAccepted', username => {
+    store.dispatch(settingsActions.usernameAccepted(username));
+});
+socket.on('settings/usernameInvalid', username => {
+    store.dispatch(settingsActions.usernameInvalid(username));
+});
+socket.on('voter/refresh', races => {
+    store.dispatch(voterActions.refresh(races));
+});
+
 ReactDOM.render(
     <Provider store={store}>
         <Router history={browserHistory}>
             <Route path="/" component={App}>
                 <Route path="w/lights" component={Lights} />
                 <Route path="w/game-echo" component={Games} />
+                <Route path="w/voter" component={Voter} />
+                <Route path="w/settings" component={Settings} />
             </Route>
         </Router>
     </Provider>,
