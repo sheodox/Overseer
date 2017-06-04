065586cfe221725a66d38bb290bedd3ea282a1f5
sheodox <imagamer27@msn.com>
2017-06-03 23:20:03 -0500

    remove redux from settings

--- src/components/index.js
+++ src/components/index.js
@@ -1,59 +1,39 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import {Router, Route, browserHistory} from 'react-router';
-import {Provider} from 'react-redux';
 import App from './App';
 import Lights from './Lights';
 import Games from './Games';
 import Voter from './Voter';
 import Settings from './Settings';
-import store from '../reducers/reducers';
-import appActions from '../actions/act-app-client';
-import settingsActions from '../actions/act-settings-client';
-import voterActions from '../actions/act-voter-client';
+const Conduit = require('../util/conduit');
 
 // socket.on('reconnect', () => {location.reload();});
 
-socket.on('disconnect', () => {
-    store.dispatch(appActions.socketDisconnected());
-});
-socket.on('connect', () => {
-    store.dispatch(appActions.socketConnected());
-});
 
-let availableSessionId = /sessionId=(\w*)/.exec(document.cookie)[1];
-store.dispatch(settingsActions.setSessionId(
-    localStorage.getItem('sessionId') || availableSessionId
-));
+const settingsConduit = new Conduit(socket, 'settings');
 
-
-let storedUsername = localStorage.getItem('username');
+const storedUsername = localStorage.getItem('username');
 if (storedUsername) {
-    let sessionId = store.getState().settings.sessionId;
-    settingsActions.propose(sessionId, storedUsername);
+    const sessionId = localStorage.getItem('sessionId') || /sessionId=(\w*)/.exec(document.cookie)[1];
+    settingsConduit.emit('propose', sessionId, storedUsername, function(username) {
+        //if we got a username back it was valid
+        if (username) {
+            localStorage.setItem('username', username);
+            localStorage.setItem('sessionId', sessionId);
+        }
+    });
 }
 
-socket.on('settings/usernameAccepted', username => {
-    store.dispatch(settingsActions.usernameAccepted(username));
-});
-socket.on('settings/usernameInvalid', username => {
-    store.dispatch(settingsActions.usernameInvalid(username));
-});
-socket.on('voter/refresh', races => {
-    store.dispatch(voterActions.refresh(races));
-});
-
 ReactDOM.render(
-    <Provider store={store}>
-        <Router history={browserHistory}>
-            <Route path="/" component={App}>
-                <Route path="w/lights" component={Lights} />
-                <Route path="w/game-echo" component={Games} />
-                <Route path="w/voter" component={Voter} />
-                <Route path="w/settings" component={Settings} />
-            </Route>
-        </Router>
-    </Provider>,
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <Route path="w/lights" component={Lights} />
+            <Route path="w/game-echo" component={Games} />
+            <Route path="w/voter" component={Voter} />
+            <Route path="w/settings" component={Settings} />
+        </Route>
+    </Router>,
     document.querySelector('#react-mount')
 );
 
