83349bd363889898c38c628d21fbea1a1c8ecdca
sheodox <imagamer27@msn.com>
2017-06-11 00:15:50 -0500

    add a component for not rendering a page requiring authentication and asks them to login

---
+++ src/components/LoginRequired.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import SVG from './SVG';
+
+const LoginRequired = React.createClass({
+    storeReturnUrl: function() {
+        sessionStorage.setItem('auth-return-url', location.pathname);
+    },
+    render: function() {
+        if (user) {
+            return this.props.children;
+        }
+        return (
+            <section className="panel">
+                <div className="panel-title">
+                    <h2>Login Required</h2>
+                    <SVG id="login-icon" />
+                </div>
+                <div className="sub-panel">
+                    <span>
+                        You need to login to access this page. <a onClick={this.storeReturnUrl} href="/auth/google">Login</a>
+                    </span>
+                </div>
+            </section>
+        )
+    }
+});
+
+module.exports = LoginRequired;
