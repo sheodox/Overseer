ba9f8cb213e174c7909b7ffc38bd319996123ab7
sheodox <imagamer27@msn.com>
2018-06-09 22:34:04 -0500

    better styling and colors on the tags. if a tag was added clicking it again will remove it from the list.

--- src/components/TagCloud.js
+++ src/components/TagCloud.js
@@ -2,21 +2,31 @@ import React from 'react';
 const formatTags = require('../util/formatters').tags;
 
 const TagCloud = React.createClass({
+    getExistingTags () {
+        const inputValue = (this.props.tagInput || {value: ''}).value.trim();
+        return inputValue ? inputValue.split(',').map(tag => tag.trim()) : [];
+    },
     tagClicked: function(tag) {
-        const inputValue = this.props.tagInput.value.trim();
-        let existingTags = inputValue ? inputValue.split(',').map(tag => tag.trim()) : [];
+        let existingTags = this.getExistingTags();
 
         if (!existingTags.includes(tag)) {
             existingTags.push(tag);
-            existingTags = formatTags(existingTags);
-            this.props.tagInput.value = existingTags.join(', ');
         }
+        else {
+            existingTags.splice(existingTags.indexOf(tag), 1);
+        }
+        existingTags = formatTags(existingTags);
+        this.props.tagInput.value = existingTags.join(', ');
         this.props.tagClicked();
     },
     render: function() {
-        const tags = (this.props.tags || []).map((tag, index) => {
-            return <a data-tag={tag} key={index} className="tag-suggestion" onClick={() => this.tagClicked(tag)}><span className="plus-character">+</span>{tag}</a>;
-        });
+        const existing = this.getExistingTags(),
+            tags = (this.props.tags || []).map((tag, index) => {
+                const selected = existing.includes(tag);
+                return <a data-tag={tag} key={index} className={'tag-suggestion' + (selected ? ' selected' : '')} onClick={() => this.tagClicked(tag)}>
+                    <span className="plus-character">{selected ? '-' : '+'}</span>{tag}
+                    </a>;
+            });
         return (
             <div className="tag-cloud">
                 {tags}
