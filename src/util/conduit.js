ff9e386c3ac95f9bac1432c6231dc94ed710a46c
sheodox <imagamer27@msn.com>
2018-07-06 22:54:31 -0500

    change Conduit into a class instead of prototype

--- src/util/conduit.js
+++ src/util/conduit.js
@@ -1,17 +1,17 @@
-function Conduit(socket, name) {
-    this.name = name;
-    this.socket = socket;
-    this.subs = [];
-    //store event handler settings for removing listeners later
-    this.bindings = [];
-}
-Conduit.prototype = {
-    sub: function(subname) {
+class Conduit {
+    constructor(socket, name) {
+        this.name = name;
+        this.socket = socket;
+        this.subs = [];
+        //store event handler settings for removing listeners later
+        this.bindings = [];
+    }
+    sub(subname) {
         const sub =  new Conduit(socket, name + '/' + subname);
         this.subs.push(sub);
         return sub;
-    },
-    on: function(bindings) {
+    }
+    on(bindings) {
         for (let rawName in bindings) {
             if (bindings.hasOwnProperty(rawName)) {
                 const eventName = this.name + ':' + rawName,
@@ -20,13 +20,23 @@ Conduit.prototype = {
                 this.bindings.push([eventName, fn]);
             }
         }
-    },
-    emit: function() {
-        const event = this.name + ':' + arguments[0],
-            rest = [].slice.call(arguments, 1);
-        this.socket.emit.apply(this.socket, [event, ...rest]);
-    },
-    destroy: function() {
+    }
+
+    /**
+     * Emit on the specified socket
+     * @param socket
+     * @param eventName
+     * @param args
+     * @private
+     */
+    _send(socket, eventName, ...args) {
+        const event = this.name + ':' + eventName;
+        socket.emit(event, ...args);
+    }
+    emit(...args) {
+        this._send(this.socket, ...args);
+    }
+    destroy() {
         this.subs.forEach((sub) => {
             sub.destroy();
         });
@@ -37,6 +47,6 @@ Conduit.prototype = {
         this.bindings = [];
         this.subs = [];
     }
-};
+}
 
 module.exports = Conduit;
