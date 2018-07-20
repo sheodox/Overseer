fda2b880180481f86df1f464df093091cfabcc3b
sheodox <imagamer27@msn.com>
2018-07-20 15:39:11 -0500

    get rid of old username proposal code because it's handled by the google oauth

--- src/routes/settings.js
+++ src/routes/settings.js
@@ -1,13 +1,8 @@
-import User from '../users';
+import Users from '../users';
 const Conduit = require('../util/conduit');
 
 export default function(io) {
     io.on('connection', socket => {
         const settingsConduit = new Conduit(socket, 'settings');
-        settingsConduit.on({
-            propose: (id, username, ack) => {
-                ack(User.register(id, username));
-            }
-        });
     });
 }
