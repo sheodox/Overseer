31802a7caafd01f5bfdfd9713653f89783bb5c79
sheodox <imagamer27@msn.com>
2018-10-04 16:06:35 -0500

    add checker pattern that fades in and out based on simplex noise

--- src/components/Trancemaker.js
+++ src/components/Trancemaker.js
@@ -46,14 +46,17 @@ class Trancemaker {
             uExistingColor: {value: this.randomColor()},
             uNewColor: {value: this.randomColor()},
             lightPos: {value: new THREE.Vector3(0, 20, 0)},
+            //color changing
             uColorFadeTime: {type: 'f', value: this.colorFadeTime},
             uColorFadeCompletion: {type: 'f', value: this.colorFadeTime},
             uResolution: {value: new THREE.Vector3(iw, ih, 1)},
             uMouse: {value: new THREE.Vector3(0, 0, 0)},
+            //rgb shifting
             uRandomY: {value: 0, type: 'f'},
             uRandomX: {value: 0, type: 'f'},
             uShiftYInterval: {value: 0, type: 'f'},
-            uShiftXInterval: {value: 0, type: 'f'}
+            uShiftXInterval: {value: 0, type: 'f'},
+
         };
 
         let nextColorChange = 0;
@@ -71,6 +74,7 @@ class Trancemaker {
                 return this.uniforms[uniformName].value;
             }
         };
+        this.uniform = uniform;
 
         let delta = 5,
             glitchUntil = null;
@@ -212,10 +216,6 @@ class Trancemaker {
                     varying vec4 vNormal;
                     varying vec3 vPos;
                     
-                    float fractionalRound(in float f, in float denominator) {
-                        return float(f * denominator) / denominator;
-                    }
-                    
                     float when_between(float a, float b, float x) {
                         return sign(clamp(b - x, 0.0, 1.0)) - sign(clamp(a - x, 0.0, 1.0));
                     }
@@ -226,8 +226,21 @@ class Trancemaker {
                     float not(float x) {
                         return 1.0 - x;
                     }
-
+                    
+                    float random(vec2 st) {
+                        return fract(sin(dot(st.xy,vec2(12.9898,78.233))) * 43758.5453123);
+                    }
+                    
+                    bool round_to_bool(float f) {
+                        return f > 0.5;
+                    }
+                    
+                    ${snoiseGLSL}
+                    
                     void main() {
+                        float fragY = gl_FragCoord.y;
+                        float fragX = gl_FragCoord.x;
+                        
                         //time delta shifting the color a bit
                         float damper = 0.15;
                         // float norm = abs(clamp(dot(vNormal.xyz, cameraPosition), -1.0, 0.0));
@@ -242,15 +255,22 @@ class Trancemaker {
                         vec3 c = mix(uExistingColor, uNewColor, step(0.5, uColorFadeCompletion / (length(vPos) / length(vec2(30.0, 30.0)))));
                         
                         float shiftingColor = mod(delta * uRandomY, 3.0);
-                        float colorShiftEnabled = when_between(uRandomY - uShiftYInterval, uRandomY, gl_FragCoord.y) *
-                            when_between(uRandomX - uShiftXInterval, uRandomX, gl_FragCoord.x);
-                        vec3 cChange = colorShiftEnabled * vec3(c.x * not(when_between(0.0, 1.0, shiftingColor)),
+                        float colorShiftEnabled = when_between(uRandomY - uShiftYInterval, uRandomY, fragY) *
+                            when_between(uRandomX - uShiftXInterval, uRandomX, fragX);
+                        //blank out one of the color channels for the rgb glitch, randomized
+                        vec3 rgbShift = colorShiftEnabled * vec3(c.x * not(when_between(0.0, 1.0, shiftingColor)),
                             c.y * not(when_between(1.0, 2.0, shiftingColor)),
                             c.z * not(when_between(2.0, 3.0, shiftingColor))
                         );
-                        vec3 deltaOffset = not(colorShiftEnabled) * abs(vec3(sin(delta * vNormal.x) * damper, sin(delta * vNormal.y) * damper, sin(delta * vNormal.z) * damper));
                         
-                        gl_FragColor = vec4((c - cChange) - deltaOffset, 1.0);
+                        vec3 normalColorChange = not(colorShiftEnabled) * abs(vec3(sin(delta * vNormal.x) * damper, sin(delta * vNormal.y) * damper, sin(delta * vNormal.z) * damper));
+                        
+                        float checkerSize = 50.0;
+                        float checkerY = floor(fragY / checkerSize);
+                        float checkerX = floor(fragX / checkerSize);
+                        float checker = snoise(vec2(checkerX, checkerY)) * 0.1 * snoise(vec2(delta, checkerX * checkerY));
+                        
+                        gl_FragColor = vec4((c - rgbShift) - normalColorChange - checker, 1.0);
                     }`
             }),
             mesh = new THREE.Mesh(this.createLowPolyGeometry(), material);
@@ -258,5 +278,78 @@ class Trancemaker {
         this.scene.add(mesh);
     }
 }
+
+const snoiseGLSL = `
+// Description : Array and textureless GLSL 2D simplex noise function.
+//      Author : Ian McEwan, Ashima Arts.
+//  Maintainer : stegu
+//     Lastmod : 20110822 (ijm)
+//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+//               Distributed under the MIT License. See LICENSE file.
+//               https://github.com/ashima/webgl-noise
+//               https://github.com/stegu/webgl-noise
+// 
+
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec2 mod289(vec2 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec3 permute(vec3 x) {
+  return mod289(((x*34.0)+1.0)*x);
+}
+
+float snoise(vec2 v)
+  {
+  const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+                      0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+                     -0.577350269189626,  // -1.0 + 2.0 * C.x
+                      0.024390243902439); // 1.0 / 41.0
+// First corner
+  vec2 i  = floor(v + dot(v, C.yy) );
+  vec2 x0 = v -   i + dot(i, C.xx);
+
+// Other corners
+  vec2 i1;
+  //i1.x = step( x0.y, x0.x ); // x0.x > x0.y ? 1.0 : 0.0
+  //i1.y = 1.0 - i1.x;
+  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+  // x0 = x0 - 0.0 + 0.0 * C.xx ;
+  // x1 = x0 - i1 + 1.0 * C.xx ;
+  // x2 = x0 - 1.0 + 2.0 * C.xx ;
+  vec4 x12 = x0.xyxy + C.xxzz;
+  x12.xy -= i1;
+
+// Permutations
+  i = mod289(i); // Avoid truncation effects in permutation
+  vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+		+ i.x + vec3(0.0, i1.x, 1.0 ));
+
+  vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+  m = m*m ;
+  m = m*m ;
+
+// Gradients: 41 points uniformly over a line, mapped onto a diamond.
+// The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
+
+  vec3 x = 2.0 * fract(p * C.www) - 1.0;
+  vec3 h = abs(x) - 0.5;
+  vec3 ox = floor(x + 0.5);
+  vec3 a0 = x - ox;
+
+// Normalise gradients implicitly by scaling m
+// Approximation of: m *= inversesqrt( a0*a0 + h*h );
+  m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+
+// Compute final noise value at P
+  vec3 g;
+  g.x  = a0.x  * x0.x  + h.x  * x0.y;
+  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+  return 130.0 * dot(m, g);
+}
+`;
 
 module.exports = Trancemaker;
