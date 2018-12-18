31c79578b35d266ab1b00a72bb25fb0f7b377ca1
sheodox <imagamer27@msn.com>
2018-12-17 22:41:02 -0600

    use FrameScaler from its new file, add a trancemaker class to the canvas element

--- src/components/Trancemaker.js
+++ src/components/Trancemaker.js
@@ -1,36 +1,8 @@
 const THREE = require('three'),
-    twopi = 2 * Math.PI;
+    twopi = 2 * Math.PI,
+    FrameScaler = require('./FrameScaler');
 function getGLSL(name) {
     return document.querySelector(`script#${name}`).textContent;
-}
-/**
- * Helper class to scale numbers to 60fps so even at faster framerates animations don't speed up
- */
-class FrameScaler {
-    constructor() {
-        this.tick();
-        this._frameTimeBase = 1000 / 60;
-    }
-    tick() {
-        const now = Date.now();
-        this._lastFrameTime = now - this._lastTick;
-        this._lastTick = now;
-    }
-
-    /**
-     * For making things smaller based on frame time, like deltas, so things don't change too much
-     * @returns {number}
-     */
-    frameTimeScaler() {
-        return this._lastFrameTime / this._frameTimeBase;
-    }
-
-    /**
-     * For making things bigger based on frame time, like random chance, so rare things don't happen too often
-     */
-    frameTimeScalerInverse() {
-        return 1 / this.frameTimeScaler();
-    }
 }
 
 class Trancemaker {
@@ -53,6 +25,7 @@ class Trancemaker {
         const backgroundColor = 0x272c38;
         this.renderer.setClearColor(backgroundColor);
         document.body.appendChild(this.renderer.domElement);
+        this.renderer.domElement.classList.add('trancemaker');
 
         window.addEventListener('resize', () => {
             iw = window.innerWidth;
