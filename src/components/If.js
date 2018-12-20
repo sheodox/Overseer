de421e312dc9cccef00f553ccb9c8321dc42bcc1
sheodox <imagamer27@msn.com>
2018-12-20 17:33:50 -0600

    add a showWhen option that functions similarly to renderWhen, but just hides/shows a parent div instead of just not including it in the markup so that the fields can be interacted with before they're shown

--- src/components/If.js
+++ src/components/If.js
@@ -5,10 +5,22 @@ class If extends React.Component {
         super(props);
     }
     render() {
-        if (!this.props.renderWhen) {
-            return null;
+        const hasProp = prop => this.props.hasOwnProperty(prop);
+        
+        if (hasProp('renderWhen')) {
+            if (!this.props.renderWhen) {
+                return null;
+            }
+            return this.props.children;
         }
-        return this.props.children;
+        
+        //to not change structure between showing and not, return a containing div either way if using show
+        if (hasProp('showWhen')) {
+            const className = this.props.showWhen ? '' : 'hidden';
+            return <div className={className}>
+                {this.props.children}
+            </div>
+        }
     }
 
 }
