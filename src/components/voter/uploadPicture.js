264266ef716ef91a7aef4a6a470719882e22e334
sheodox <3468630+sheodox@users.noreply.github.com>
2020-01-24 21:59:26 -0600

    show progress when uploading an image so you know something is happening instead of just waiting for it to appear eventually

--- src/components/voter/uploadPicture.js
+++ src/components/voter/uploadPicture.js
@@ -1,17 +1,39 @@
-function uploadPicture(race_id, candidate_id, file) {
-	fetch(`/voter/${race_id}/${candidate_id}/upload`, {
-		method: 'POST',
-		body: file
-	})
-		.then(response => {
-			const errorToast = msg => Toaster.add({type: 'text', text: 'Error! ' + msg});
+const axios = require('axios'),
+	formatters = require('../../util/formatters');
 
-			if(response.status === 413) {
-				errorToast('That image is too big!');
+function uploadPicture(race_id, candidate_id, file) {
+	const progressToast = Toaster.add({
+		title: 'Voter - Uploading Image',
+		type: 'progress'
+	});
+
+	axios
+		.request({
+			method: 'POST',
+			url: `/voter/${race_id}/${candidate_id}/upload`,
+			data: file,
+			headers: {
+				'Content-type': file.type
+			},
+			onUploadProgress(e) {
+				progressToast({
+					value: e.loaded,
+					max: e.total,
+					text: `${formatters.bytes(e.loaded, 'mb')} mb / ${formatters.bytes(e.total, 'mb')} mb`
+			})
 			}
-			else if (!response.ok) {
-				errorToast(response.statusText)
-			}
+		})
+		.then(() => {
+			//if the old progress toast is still around, hide that and replace it with a new one
+			progressToast({
+				hidden: true
+			});
+
+			Toaster.add({
+				type: 'text',
+				title: 'Voter',
+				text: 'Image uploaded!'
+			});
 		});
 }
 
