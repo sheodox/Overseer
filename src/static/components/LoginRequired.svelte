72462dbeda0d39b2f3c9b0f3afac02f01a99e83d
sheodox <3468630+sheodox@users.noreply.github.com>
2021-03-20 22:04:05 -0500

    Make the sign in page a tiny bit nicer looking. It's still pretty bare-bones but I don't think it needs to be anything more than that.

--- src/static/components/LoginRequired.svelte
+++ src/static/components/LoginRequired.svelte
@@ -1,3 +1,12 @@
-<div class="page-content">
-    <a href="/auth/google">You need to login to access this page.</a>
+<div class="text-align-center">
+    <p>You need to login to access this page.</p>
+
+    <a href="/auth/google">
+        <Icon icon="sign-in-alt" />
+        Sign In
+    </a>
 </div>
+
+<script>
+    import {Icon} from 'sheodox-ui';
+</script>
