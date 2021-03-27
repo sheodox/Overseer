248a806f11377e3121d20cedcf45e2b15202b3bc
sheodox <3468630+sheodox@users.noreply.github.com>
2021-03-26 21:38:18 -0500

    Added a way to attach files by just dragging a zip over the page. You can attach a zip by dragging over the file input so this just makes it real big and invisible over the whole page and show a message when you start dragging.

--- src/static/components/echo/EchoFileSelect.svelte
+++ src/static/components/echo/EchoFileSelect.svelte
@@ -7,9 +7,33 @@
         cursor: default;
         color: var(--muted);
     }
+    .drop-message, .dragging input {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100vw;
+        height: 100vh;
+    }
+    .drop-message {
+        z-index: 9999;
+        background: rgba(0, 0, 0, 0.9);
+        font-size: 4rem;
+    }
+    .dragging input {
+        z-index: 10000;
+        display: block;
+        opacity: 0;
+    }
 </style>
 
-<div>
+{#if dragging}
+    <div class="drop-message f-column justify-content-center align-items-center">
+        <p>Drop to attach file!</p>
+    </div>
+{/if}
+<div class:dragging={dragging}>
     <label class="button" class:disabled={!$echoOnline} title={!$echoOnline ? 'Echo is offline!' : ''}>
         <Icon icon="upload" />
         {mode === 'upload' ? 'Select a file' : '(Optional) Upload updated file'}
@@ -26,6 +50,13 @@
     </span>
 </div>
 
+<svelte:body
+    on:dragover={() => dragging = true}
+    on:drop={() => dragging = false}
+    on:mouseleave={() => dragging = false}
+    on:dragexit={() => dragging = false}
+/>
+
 <script>
     import {Icon} from 'sheodox-ui';
     import {bytes} from "../../../shared/formatters";
@@ -33,7 +64,8 @@
 
     export let mode; // 'upload' | 'edit', EchoUpload's "mode"
     export let file;
-    let files = [];
+    let files = [],
+        dragging = false;
 
     $: file = files[0];
 </script>
