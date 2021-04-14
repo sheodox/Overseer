d81943271b1fa10cd8ef673a1ad33d21ecf19c89
sheodox <3468630+sheodox@users.noreply.github.com>
2021-04-13 20:05:18 -0500

    Dispatch a 'followed' event when a link is clicked. This is used by the notification tray to mark a notification as read when it's clicked.

--- src/static/components/Link.svelte
+++ src/static/components/Link.svelte
@@ -5,14 +5,21 @@
 </style>
 <a
     href={href}
-    on:click|preventDefault={() => page(href)}
+    on:click|preventDefault={follow}
     class:no-hover-styles={noHoverStyles}
 >
     <slot />
 </a>
 
 <script>
+    import {createEventDispatcher} from 'svelte';
     import page from 'page';
     export let href
     export let noHoverStyles;
+    const dispatch = createEventDispatcher();
+
+    function follow() {
+        page(href)
+        dispatch('followed');
+    }
 </script>
