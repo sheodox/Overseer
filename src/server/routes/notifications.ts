4e746f10cc57661b92d2dde8c72b1b4f3c9050c2
sheodox <3468630+sheodox@users.noreply.github.com>
2021-04-13 19:56:29 -0500

    Added routes/socket connections for notifications. The messages here aren't easy to send diffs because notifications are per-user, so the syncing between clients is more surgical and requires a bit of client side data manipulation (for read states) than the data in the other apps. There's also a route that takes a notification ID, marks it as read, and the redirects to that notification's href, so once push notifications are in it can mark the notification as read on the way into the site.

---
+++ src/server/routes/notifications.ts
@@ -0,0 +1,53 @@
+import {Router} from 'express';
+import {notifications} from "../db/notifications";
+import {AppRequest} from "../types";
+import {Envoy} from "../../shared/envoy";
+import {createSafeWebsocketHandler, Harbinger} from "../util/harbinger";
+import {appBooker} from "../db/booker";
+import {appLogger} from "../util/logger";
+import {io} from '../server';
+
+export const router = Router();
+
+//follow a notification redirect, and mark the notification as read automatically!
+//users can get here by clicking push notifications
+router.get('/n/:notificationId', async (req: AppRequest, res, next) => {
+    //mark the notification as read and go to wherever they were supposed to lead
+    const notification = await notifications.markRead(req.params.notificationId)
+    notificationHarbinger.broadcastToUser('markedRead', req.user.id, notification.id);
+    res.redirect(notification.href || '/');
+})
+
+const notificationHarbinger = new Harbinger('notifications');
+io.on('connection', socket => {
+    const notificationsEnvoy = new Envoy(socket, 'notifications'),
+        userId = Harbinger.getUserId(socket);
+
+    //don't attempt to let users who aren't signed in to connect to the websocket
+    if (!userId) {
+        return;
+    }
+
+    const checkPermission = createSafeWebsocketHandler(
+        userId,
+        appBooker,
+        socket,
+        appLogger
+    );
+
+    notificationsEnvoy.on({
+        init: checkPermission('notifications', async done => {
+            done(
+                await notifications.getNotifications(userId)
+            )
+        }),
+        markRead: checkPermission('notifications', async id => {
+            await notifications.markRead(id);
+            notificationHarbinger.broadcastToUser('markedRead', userId, id);
+        }),
+        markAllRead: checkPermission('notifications', async () => {
+            await notifications.markAllRead(userId);
+            notificationHarbinger.broadcastToUser('markedAllRead', userId);
+        })
+    });
+})
