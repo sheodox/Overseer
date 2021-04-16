30f642b98f92ffa5a8dddb08bbeb01d766610cd4
sheodox <3468630+sheodox@users.noreply.github.com>
2021-04-16 18:52:50 -0500

    Prettier event times. This will show "today" instead of a date wherever a date is shown.

--- src/static/components/events/EventTimes.svelte
+++ src/static/components/events/EventTimes.svelte
@@ -1,22 +1,38 @@
 <style>
+    p::first-letter {
+        text-transform: capitalize;
+    }
 </style>
 
 <p>
     {#if event.eventDays.length === 1}
-        On {event.startDate.toLocaleDateString()} from {prettyTime(event.startDate)} to {prettyTime(event.endDate)}
+        {event.startDate.toLocaleDateString()} from {prettyTime(event.startDate)} to {prettyTime(event.endDate)}
     {:else}
-        From {prettyDate(event.startDate)} to {prettyDate(event.endDate)}
+        {prettyDate(event.startDate)} to {prettyDate(event.endDate)}
     {/if}
 </p>
-
 <script>
+    import {onDestroy} from 'svelte';
     export let event;
+    let now = Date.now();
 
     const dateFormat = new Intl.DateTimeFormat('en-US', {dateStyle: 'short', timeStyle: 'short'}),
         timeFormat = new Intl.DateTimeFormat('en-US', {timeStyle: 'short'});
 
+    $: prettyStartDate = event.startDate.toLocaleDateString();
+    $: startDay = prettyStartDate === new Date(now).toLocaleDateString() ? 'Today' : `On ${prettyStartDate}`;
+
+    const interval = setInterval(() => {
+        now = Date.now();
+    }, 1000 * 60);
+
+    onDestroy(() => {
+        clearInterval(interval);
+    })
+
     function prettyDate(date) {
-        return dateFormat.format(date);
+        const isToday = date.toLocaleDateString() === new Date().toLocaleDateString();
+        return isToday ? `today at ${prettyTime(date)}` : dateFormat.format(date);
     }
 
     function prettyTime(date) {
