2c1b42b4870e68a62a730ed1bd3b6ccfdb7f7433
sheodox <imagamer27@msn.com>
2021-11-12 06:55:44 -0600

    Changed the vite build config to set the outDir on vite's settings, not the nested rollup settings. The way it was made it so the `dist` directory gets clearned with the static build even though it's not supposed to go there.

--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,8 @@ export default defineConfig({
 	},
 	build: {
 		manifest: true,
+		outDir: './public-dist',
 		rollupOptions: {
-			output: {
-				dir: './public-dist',
-			},
 			input: {
 				main: '/src/static/main.ts',
 				admin: '/src/static/admin/admin-main.ts',
