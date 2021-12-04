fe82f055811b8cf32cd8d14a694a4357a183066b
sheodox <imagamer27@msn.com>
2021-12-04 09:55:31 -0600

    Send 'ok' from the internal server's / route, so it's not just an empty 200

--- src/server/internal-server.ts
+++ src/server/internal-server.ts
@@ -29,7 +29,7 @@ app.get('/logs', (req: AppRequest, res, next) => {
 
 app.get('/', (req, res) => {
 	// respond to / with a success for health checks
-	res.send();
+	res.send('ok');
 });
 
 app.use((req, res, next) => {
