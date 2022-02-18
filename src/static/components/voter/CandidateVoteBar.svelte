5f9e152bb188e5a1c02d8999e7116217afd2764e
sheodox <imagamer27@msn.com>
2022-02-18 15:42:53 -0600

    Added a width transition on vote bars
    Both bars need to be there for 0->1 transition to animate too.

--- src/static/components/voter/CandidateVoteBar.svelte
+++ src/static/components/voter/CandidateVoteBar.svelte
@@ -9,10 +9,15 @@
 		&.has-opposing {
 			border-right-width: 0;
 		}
+		&.no-opposing {
+			border-radius: 0 3px 3px 0;
+		}
 	}
 	.down {
 		border-color: var(--shdx-accent-red);
 		background-color: var(--shdx-red-800);
+		border-radius: 0 3px 3px 0;
+
 		&.has-opposing {
 			border-left-width: 0;
 		}
@@ -24,18 +29,30 @@
 	.vote-count {
 		padding: 0 0.2rem;
 	}
+	.bar {
+		transition: width 0.2s;
+
+		&.no-votes {
+			border: none;
+		}
+	}
 </style>
 
-{#if votePercent > 0}
-	<div class={`f-row ${direction} ${hasOpposingVoters ? 'has-opposing' : ''}`} style="width: {votePercent}%">
+<div
+	class={`bar f-row ${direction} ${hasOpposingVoters ? 'has-opposing' : 'no-opposing'} ${
+		votePercent > 0 ? 'has-votes' : 'no-votes'
+	}`}
+	style="width: {votePercent}%"
+>
+	{#if votePercent > 0}
 		<div class="voters f-row gap-1">
 			<span class="vote-count fw-bold">{votes}</span>
 			{#each voters as userId (userId)}
 				<UserBubble {userId} mode="minimal" />
 			{/each}
 		</div>
-	</div>
-{/if}
+	{/if}
+</div>
 
 <script lang="ts">
 	import UserBubble from '../UserBubble.svelte';
